083606cd2b7c5a91c47ab7b7d02494c0f3926f94
Dhanavardhini Alagar <131867897+Dhanavardhini@users.noreply.github.com>
2024-11-08 15:58:17 +0530

    Implemented Responsive Value Addition Form with Complete Device Support
    This project is a responsive "Add Value" interface built with HTML, external CSS, Bootstrap, and dynamic JavaScript functionality. It allows users to input values dynamically and manage them in an interactive form.
    
    ### Key Features
    - **Responsive Design**: The layout is fully responsive across all device sizes (sm, md, lg, xl, xxl), with elements that adapt and center seamlessly for an optimized user experience.
    - **Interactive Elements**: Includes input fields and buttons styled with external CSS and Bootstrap, with smooth transitions between breakpoints.
    - **Dynamic JavaScript**: Implements JavaScript functionality to handle user input, including adding and clearing values.
    
    The project demonstrates a blend of front-end development skills with a focus on usability and responsiveness.

---
+++ add value.js
@@ -0,0 +1,21 @@
+function dhana() {
+
+   let a =document.getElementById("ammu").value;
+
+   let b=document.getElementById("a1");
+   
+   let c= document.createElement("H1");
+ 
+   c.innerText=a;
+   b.appendChild(c)
+
+}
+dhana()
+function d1() {
+   
+   let b=document.getElementById("a1").innerHTML=""
+
+   let a=document.getElementById("ammu"). value="";
+}
+
+// ---------------------------------
